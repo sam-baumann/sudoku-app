38c3477668a01d30a9ae251f7fb108abc7af8de9
Sam Baumann <sbauman2@vols.utk.edu>
2025-04-02 22:23:29 -0400

    add reactivity to board

--- src/main/sambaumann/sudoku/app.cljs
+++ src/main/sambaumann/sudoku/app.cljs
@@ -1,7 +1,12 @@
 (ns sambaumann.sudoku.app
-  (:require [reagent.dom.client :as rdom]))
+  (:require [reagent.dom.client :as rdom]
+            [reagent.core :as r]))
 
 (defonce root (rdom/create-root (js/document.getElementById "root")))
+
+(defonce active-square (r/atom [0 0]))
+
+(defonce board-state (r/atom (into [] (repeat 9 (into [] (repeat 9 0))))))
 
 (defn grid
   "creates the grid"
@@ -12,9 +17,8 @@
                 (into [:tr]
                       (map (fn [j]
                              [:td {:className "cell"
-                                   :onClick #(js/alert
-                                              (str "Cell " i ", " j " clicked"))}
-                              \" i j \"])
+                                   :onClick #(swap! board-state assoc-in [i j] 5)}
+                              (get-in @board-state [i j])])
                            (range 9))))
               (range 9)))])
 
@@ -22,4 +26,4 @@
   (grid))
 
 (defn ^:export init []
-  (rdom/render root (app)))
+  (rdom/render root (r/as-element [app])))
