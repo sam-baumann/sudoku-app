ffdb95f21416a117d62bbc160bd72670ba47fbba
Sam Baumann <sbauman2@vols.utk.edu>
2025-05-06 12:36:00 -0400

    begin norvig implementation

--- src/main/sambaumann/sudoku/app.cljs
+++ src/main/sambaumann/sudoku/app.cljs
@@ -1,145 +1,64 @@
 (ns sambaumann.sudoku.app
   (:require [reagent.dom.client :as rdom]
             [reagent.core :as r]
-            [goog.events :as events]))
+            [goog.events :as events]
+            [clojure.set :as set]))
 
 ;forward declare atoms used in the game logic and UI logic. 
 (declare active-square board-state original-state blank-puzzle)
 
+;forward declare fill so it can be used in constrain
+(declare fill)
+
 ;Game Logic
-(defn check-puzzle
-  ;checks the puzzle against the known correct, returns true or false and updates the error cells atom
-  []
-  false)
+;reimplement peter norvig's sudoku solver in clojurescript; mine wasn't efficient enough
 
-(defn get-row
-  ;checks the individual row
-  [puzzle row-index]
-  (get-in puzzle [row-index]))
+(defn cross
+  [A B]
+  (for [a A
+        b B]
+    (+ a b)))
 
-(defn get-col
-  ;check column for correctness
-  [puzzle col-index]
-  (map #(get % col-index) puzzle))
+(def digits "123456789")
 
-(defn get-group
-  [puzzle group-index]
-  (let [starti (* 3 (quot group-index 3))
-        startj (* 3 (mod group-index 3))]
-    (apply concat
-           (map
-            (fn [i] (map (fn [j]
-                           (get-in puzzle [i j]))
-                         (range startj (+ 3 startj))))
-            (range starti (+ 3 starti))))))
+(def rows "ABCDEFGHI")
 
-(defn is-full
-  [puzzle-component]
-  ; a row is full if it has no zeroes
-  (= 0 (count (filter zero? puzzle-component))))
+(def cols digits)
 
-(defn all-unique
-  [puzzle-component]
-  (let
-   [filled-entries (remove zero? puzzle-component)]
-    (= (distinct filled-entries) filled-entries)))
+(def squares (cross rows cols))
 
-(defn is-solved
-  [puzzle]
-  ;ensure all rows,cols, and groups are fully filled and unique
-  (every? true?
-          (map
-           (fn [i] (every? true?
-                           (map #(every? true?
-                                         (list (is-full %) (all-unique %)))
-                                (list (get-row puzzle i) (get-col puzzle i) (get-group puzzle i)))))
-           (range 9))))
+(def all-boxes
+  (for [rs ["ABC" "DEF" "GHI"]
+        cs ["123" "456" "789"]]
+    (cross rs cs)))
 
-(defn group-from-coords
-  [i j]
-  (+ (* 3 (quot i 3)) (quot j 3)))
+(def all-units
+  (concat (for [c cols] (set (cross rows c)))
+          (for [r rows] (set (cross r cols)))
+          (map set all-boxes)))
 
-(defn valid-entry
-  ;returns wether inserting value into puzzle at [i j] is valid
-  [puzzle value i j]
-  (let
-   [new-puzzle (assoc-in puzzle [i j] value)]
-    (every? true? (map all-unique [(get-row new-puzzle i) (get-col new-puzzle j) (get-group new-puzzle (group-from-coords i j))]))))
+(def units
+  (into {} (for [s squares]
+             [s (for [u all-units
+                      :when (contains? u s)] u)])))
 
-(defn all-pairs
-  []
-  (apply concat (map (fn [i] (map (fn [j] [i j]) (range 9))) (range 9))))
+(def peers
+  (into {} (for [s squares]
+             [s (disj (apply set/union (get units s)) s)])))
 
-(defn empty-cells
-  [puzzle]
-  (filter #(= 0 (get-in puzzle %)) (all-pairs)))
+(defn is-solution
+  [solution puzzle]
+  (and solution ;solution is not none
+       (every? true? (for [s squares]
+                       (contains? (get puzzle s) (get solution s)))) ;ensure solution fits in the original puzzle
+       (every? true? (for [unit all-units]
+                       (= (set (for [s unit] (get solution s))) (set digits)))))) ;ensure all units have each digit filled once
 
-(defn constraints
-  [puzzle]
-  ;return a vector of vector of sets representing the valid next states of the given puzzle
-  (into [] (map (fn [i] (into [] (map (fn [j] (set (filter #(valid-entry puzzle % i j) (range 1 10)))) (range 9)))) (range 9))))
+(defn constrain
+  [grid])
 
-(defn count-constraints
-  [puzzle constraint]
-  (remove nil?
-          (apply concat
-                 (map (fn [i] (map (fn [j] (if (= (get-in puzzle [i j]) 0)
-                                             [i j (count (get-in constraint [i j]))]
-                                             nil)) (range 9))) (range 9)))))
-
-(defn most-constrained-cells
-  ;takes in constraints, and returns the positions and possible values of the most constrained cells
-  ;(only unfilled ones)
-  [puzzle constraint]
-  (let [constraint-counts (count-constraints puzzle constraint)
-        min-count (apply min (map #(nth % 2) constraint-counts))]
-    (filter #(= (nth % 2) min-count) constraint-counts)))
-
-(defn next-puzzles
-  [puzzle constraint]
-  (map #(vector (first %) (second %) (nth % 2) (assoc-in puzzle [(first %) (second %)] (nth % 2)))
-       (mapcat #(map (fn [i] [(first %) (second %) i]) (get-in constraint [(first %) (second %)])) (shuffle (most-constrained-cells puzzle constraint)))))
-
-(defn reduce-constraint
-  [i j val constraint]
-  (let [group_starti (* 3 (quot i 3))
-        group_startj (* 3 (quot j 3))
-        coords-to-edit (set (concat
-                             (for [x (range 9)] [i x])
-                             (for [x (range 9)] [x j])
-                             (for [x (range group_starti (+ 3 group_starti))
-                                   y (range group_startj (+ 3 group_startj))] [x y])))]
-    (reduce (fn [cons coord] (update-in cons coord disj val)) constraint coords-to-edit)))
-
-(defn check-each-valid
-  ;ensure each cell still has a valid value
-  [puzzle constraints]
-  (not (some #(and (= (get-in puzzle %) 0)
-                   (= (count (get-in constraints %)) 0))
-             (for [x (range 9)
-                   y (range 9)]
-               [x y]))))
-
-(defn recursive-solve-puzzle
-  ;returns solution to the given puzzle 
-  [puzzle blanks visited constraint]
-  ;if already visited, back out
-  (lazy-seq
-   (if (@visited puzzle) []
-       (do (swap! visited conj puzzle)
-           (cond
-             (= blanks 0) [puzzle]
-             (not (check-each-valid puzzle constraint)) []
-             :else (mapcat (fn [next-puzzle] (recursive-solve-puzzle
-                                              (nth next-puzzle 3)
-                                              (- blanks 1)
-                                              visited
-                                              (reduce-constraint (first next-puzzle) (second next-puzzle) (nth next-puzzle 2) constraint)))
-                           (next-puzzles puzzle constraint)))))))
-
-(defn solve-puzzle
-  [puzzle]
-  (recursive-solve-puzzle puzzle (count (empty-cells puzzle)) (atom #{}) (constraints puzzle)))
+(defn fill
+  [grid s d])
 
 (defn blank-puzzle
   []
@@ -225,8 +144,6 @@
 
 ;the current digits in the board - 0 if not filled
 (defonce board-state (r/atom (blank-puzzle)))
-(add-watch board-state :check-solved
-           (fn [] (when (is-solved @board-state) (js/console.log "solved!"))))
 
 ;the puzzle is loaded in as this atom 
 (defonce original-state (r/atom (blank-puzzle)))
